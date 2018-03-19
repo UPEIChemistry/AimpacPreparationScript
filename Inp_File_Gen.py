2cf935dc888a0faf2a61890978ab553f52d20f9c
Mat <m.c.larade@gmail.com>
2018-03-19 13:42:14 -0300

    Added a test script and improved readabity of all scripts

--- Inp_File_Gen.py
+++ Inp_File_Gen.py
@@ -1,21 +1,24 @@
 import os
 
-filelist = os.listdir(os.curdir).sort()
+filelist = os.listdir(os.curdir)
+filelist.sort()
 for file in filelist:
         if file.endswith(".wfn"):
                 with open(file, 'r') as infile:
                         lines = infile.readlines()
                 for i, line in enumerate(lines):
                         if i > 1:
-                                nospaceline = line.split()
-                                if nospaceline[0] == 'CENTRE':
+                                splitline = line.split()
+                                atomtype = splitline[0]
+                                atomnumber = splitline[1]
+                                if atomtype == 'CENTRE':
                                         break
-                                outfile = open(file + nospaceline[0] + nospaceline[1] + '.inp', 'w')
+                                outfile = open(file + atomtype + atomnumber + '.inp', 'w')
 # This is to make sure there are 3 & 4 spaces when appropriate
-                                if len(nospaceline[1]) == 2:
-                                        outfile.write(nospaceline[0] + nospaceline[1] + '\n' + '  ' + nospaceline[0] + '   ' + nospaceline[1])
+                                if len(atomnumber) == 2:
+                                        outfile.write(atomtype + atomnumber + '\n' + '  ' + atomtype + '   ' + atomnumber)
                                 else:
-                                        outfile.write(nospaceline[0] + nospaceline[1] + '\n' + '  ' + nospaceline[0] + '    ' + nospaceline[1])
+                                        outfile.write(atomtype + atomnumber + '\n' + '  ' + atomtype + '    ' + atomnumber)
 # These are options for aimpac, please consult the domcumentation for aimpac to understand what this is for
                                 outfile.write('\nPROMEGA\n128 96 192\n')
                                 outfile.write('OPTIONS\nINTEGER 2\n')
